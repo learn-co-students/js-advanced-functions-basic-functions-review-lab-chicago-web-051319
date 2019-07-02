117fdc47a3d9ab2cd00816c35d4dc0252ca888e7
Derek Townsend <derekjavon2@gmail.com>
2019-07-02 15:22:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function saturdayFun(funThing = "roller-skate") {
+  return `This Saturday, I want to ${funThing}!`;
+}
+
+let mondayWork = function (funThing = "go to the office") {
+  return `This Monday, I will ${funThing}.`;
+}
+
+function wrapAdjective(wrapChar = "*") {
+  return function (complement = "special") {
+    return `You are ${wrapChar}${complement}${wrapChar}!`
+  }
+}
+
+const Calculator = {
+  add: function(num1, num2){
+    return num1+num2
+  },
+  subtract: function(num1, num2){
+    return num1-num2
+  },
+  multiply: function(num1, num2){
+    return num1*num2
+  },
+  divide: function(num1, num2){
+    return num1/num2
+  },
+}
+
+function actionApplyer(starting, functions) {
+  // console.log(functions);
+  if (!functions.length == 0) {
+    return functions.reduce( (total, element) =>{
+      return element(total)
+    },starting)
+  }
+  return starting
+}
