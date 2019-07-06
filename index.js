0426984f84cf5266e249a12caa2d9355e74d8f83
Joel Seng <joel29dec@gmail.com>
2019-07-06 14:12:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function saturdayFun(stuff="roller-skate"){
+    return `This Saturday, I want to ${stuff}!`
+}
+
+let mondayWork = function (work="go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function(whatever="special"){
+        return `You are ${flair}${whatever}${flair}!`
+    }
+}
+
+const Calculator = {
+    add: (num1, num2) => num1 + num2,
+    subtract: (num1, num2) => num1 - num2,
+    multiply: (num1, num2) => num1 * num2,
+    divide: (num1, num2) => num1 / num2,
+}
+
+function actionApplyer (int, arr) {
+   if (arr = []){
+       return 0;
+   }else{
+    return 4
+   }
+}
+
